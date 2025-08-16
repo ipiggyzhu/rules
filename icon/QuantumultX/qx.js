38543c3e2e8d0bd3c4a0551b2482baa85ee4c7d5
ipiggyzhu <ipiggyzhu@gmail.com>
2025-08-16 13:37:00 +0800

    Create qx.js

---
+++ icon/QuantumultX/qx.js
@@ -0,0 +1,406 @@
+{
+  "name": "custom icon",
+  "description": "From https://github.com/luestr/IconResource. Converted for Quantumult X.",
+  "icons": [
+    {
+      "name": "1Password",
+      "regex": "(?i)^1Password$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/1Password.png"
+    },
+    {
+      "name": "2Do",
+      "regex": "(?i)^2Do$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/2Do.png"
+    },
+    {
+      "name": "AdGuard",
+      "regex": "(?i)^AdGuard$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/AdGuard.png"
+    },
+    {
+      "name": "Affinity",
+      "regex": "(?i)^Affinity$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Affinity.png"
+    },
+    {
+      "name": "Aha",
+      "regex": "(?i)^Aha$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Aha.png"
+    },
+    {
+      "name": "Airtable",
+      "regex": "(?i)^Airtable$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Airtable.png"
+    },
+    {
+      "name": "Amazon",
+      "regex": "(?i)^Amazon$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Amazon.png"
+    },
+    {
+      "name": "Apple",
+      "regex": "(?i)^Apple$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Apple.png"
+    },
+    {
+      "name": "AppleMusic",
+      "regex": "(?i)^AppleMusic$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/AppleMusic.png"
+    },
+    {
+      "name": "AppleNews",
+      "regex": "(?i)^AppleNews$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/AppleNews.png"
+    },
+    {
+      "name": "AppleTV",
+      "regex": "(?i)^AppleTV$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/AppleTV.png"
+    },
+    {
+      "name": "Bilibili",
+      "regex": "(?i)^Bilibili$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Bilibili.png"
+    },
+    {
+      "name": "Box",
+      "regex": "(?i)^Box$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Box.png"
+    },
+    {
+      "name": "Cisco",
+      "regex": "(?i)^Cisco$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Cisco.png"
+    },
+    {
+      "name": "Clash",
+      "regex": "(?i)^Clash$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Clash.png"
+    },
+    {
+      "name": "Cloudflare",
+      "regex": "(?i)^Cloudflare$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Cloudflare.png"
+    },
+    {
+      "name": "Deepl",
+      "regex": "(?i)^Deepl$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Deepl.png"
+    },
+    {
+      "name": "Developer",
+      "regex": "(?i)^Developer$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Developer.png"
+    },
+    {
+      "name": "Discord",
+      "regex": "(?i)^Discord$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Discord.png"
+    },
+    {
+      "name": "Disney+",
+      "regex": "(?i)^Disney\\+$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Disney+.png"
+    },
+    {
+      "name": "Dropbox",
+      "regex": "(?i)^Dropbox$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Dropbox.png"
+    },
+    {
+      "name": "Facebook",
+      "regex": "(?i)^Facebook$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Facebook.png"
+    },
+    {
+      "name": "Final",
+      "regex": "(?i)^Final$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Final.png"
+    },
+    {
+      "name": "Firefox",
+      "regex": "(?i)^Firefox$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Firefox.png"
+    },
+    {
+      "name": "Gcore",
+      "regex": "(?i)^Gcore$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Gcore.png"
+    },
+    {
+      "name": "GitHub",
+      "regex": "(?i)^GitHub$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/GitHub.png"
+    },
+    {
+      "name": "Google",
+      "regex": "(?i)^Google$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Google.png"
+    },
+    {
+      "name": "GoogleDrive",
+      "regex": "(?i)^GoogleDrive$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/GoogleDrive.png"
+    },
+    {
+      "name": "HBO",
+      "regex": "(?i)^HBO$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/HBO.png"
+    },
+    {
+      "name": "Hulu",
+      "regex": "(?i)^Hulu$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Hulu.png"
+    },
+    {
+      "name": "iCloud",
+      "regex": "(?i)^iCloud$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/iCloud.png"
+    },
+    {
+      "name": "Instagram",
+      "regex": "(?i)^Instagram$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Instagram.png"
+    },
+    {
+      "name": "Intel",
+      "regex": "(?i)^Intel$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Intel.png"
+    },
+    {
+      "name": "Kakao",
+      "regex": "(?i)^Kakao$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Kakao.png"
+    },
+    {
+      "name": "Line",
+      "regex": "(?i)^Line$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Line.png"
+    },
+    {
+      "name": "LinkedIn",
+      "regex": "(?i)^LinkedIn$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/LinkedIn.png"
+    },
+    {
+      "name": "Loon",
+      "regex": "(?i)^Loon$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Loon.png"
+    },
+    {
+      "name": "Microsoft",
+      "regex": "(?i)^Microsoft$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Microsoft.png"
+    },
+    {
+      "name": "Netflix",
+      "regex": "(?i)^Netflix$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Netflix.png"
+    },
+    {
+      "name": "Notion",
+      "regex": "(?i)^Notion$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Notion.png"
+    },
+    {
+      "name": "Nvidia",
+      "regex": "(?i)^Nvidia$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Nvidia.png"
+    },
+    {
+      "name": "OneDrive",
+      "regex": "(?i)^OneDrive$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/OneDrive.png"
+    },
+    {
+      "name": "PayPal",
+      "regex": "(?i)^PayPal$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/PayPal.png"
+    },
+    {
+      "name": "Pinterest",
+      "regex": "(?i)^Pinterest$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Pinterest.png"
+    },
+    {
+      "name": "Pixiv",
+      "regex": "(?i)^Pixiv$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Pixiv.png"
+    },
+    {
+      "name": "PrimeVideo",
+      "regex": "(?i)^PrimeVideo$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/PrimeVideo.png"
+    },
+    {
+      "name": "Proxy",
+      "regex": "(?i)^Proxy$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Proxy.png"
+    },
+    {
+      "name": "PSN",
+      "regex": "(?i)^PSN$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/PSN.png"
+    },
+    {
+      "name": "Pubg",
+      "regex": "(?i)^Pubg$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Pubg.png"
+    },
+    {
+      "name": "QuantumultX",
+      "regex": "(?i)^QuantumultX$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/QuantumultX.png"
+    },
+    {
+      "name": "Reddit",
+      "regex": "(?i)^Reddit$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Reddit.png"
+    },
+    {
+      "name": "Shadowrocket",
+      "regex": "(?i)^Shadowrocket$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Shadowrocket.png"
+    },
+    {
+      "name": "Skype",
+      "regex": "(?i)^Skype$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Skype.png"
+    },
+    {
+      "name": "Slack",
+      "regex": "(?i)^Slack$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Slack.png"
+    },
+    {
+      "name": "Speedtest",
+      "regex": "(?i)^Speedtest$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Speedtest.png"
+    },
+    {
+      "name": "Spotify",
+      "regex": "(?i)^Spotify$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Spotify.png"
+    },
+    {
+      "name": "Steam",
+      "regex": "(?i)^Steam$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Steam.png"
+    },
+    {
+      "name": "Sticker",
+      "regex": "(?i)^Sticker$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Sticker.png"
+    },
+    {
+      "name": "Surge",
+      "regex": "(?i)^Surge$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Surge.png"
+    },
+    {
+      "name": "Tachiyomi",
+      "regex": "(?i)^Tachiyomi$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Tachiyomi.png"
+    },
+    {
+      "name": "Telegram",
+      "regex": "(?i)^Telegram$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Telegram.png"
+    },
+    {
+      "name": "TestFlight",
+      "regex": "(?i)^TestFlight$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/TestFlight.png"
+    },
+    {
+      "name": "TikTok",
+      "regex": "(?i)^TikTok$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/TikTok.png"
+    },
+    {
+      "name": "Tinder",
+      "regex": "(?i)^Tinder$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Tinder.png"
+    },
+    {
+      "name": "Tumblr",
+      "regex": "(?i)^Tumblr$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Tumblr.png"
+    },
+    {
+      "name": "Twitch",
+      "regex": "(?i)^Twitch$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Twitch.png"
+    },
+    {
+      "name": "Twitter",
+      "regex": "(?i)^Twitter$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Twitter.png"
+    },
+    {
+      "name": "V2ray",
+      "regex": "(?i)^V2ray$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/V2ray.png"
+    },
+    {
+      "name": "Viber",
+      "regex": "(?i)^Viber$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Viber.png"
+    },
+    {
+      "name": "Vimeo",
+      "regex": "(?i)^Vimeo$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Vimeo.png"
+    },
+    {
+      "name": "WeChat",
+      "regex": "(?i)^WeChat$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/WeChat.png"
+    },
+    {
+      "name": "Weibo",
+      "regex": "(?i)^Weibo$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Weibo.png"
+    },
+    {
+      "name": "WhatsApp",
+      "regex": "(?i)^WhatsApp$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/WhatsApp.png"
+    },
+    {
+      "name": "Wikipedia",
+      "regex": "(?i)^Wikipedia$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Wikipedia.png"
+    },
+    {
+      "name": "Windows",
+      "regex": "(?i)^Windows$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Windows.png"
+    },
+    {
+      "name": "Xbox",
+      "regex": "(?i)^Xbox$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Xbox.png"
+    },
+    {
+      "name": "Yahoo",
+      "regex": "(?i)^Yahoo$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Yahoo.png"
+    },
+    {
+      "name": "Yandex",
+      "regex": "(?i)^Yandex$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Yandex.png"
+    },
+    {
+      "name": "YouTube",
+      "regex": "(?i)^YouTube$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/YouTube.png"
+    },
+    {
+      "name": "Zhihu",
+      "regex": "(?i)^Zhihu$",
+      "url": "https://raw.githubusercontent.com/luestr/IconResource/main/IconSet/KeLee/Zhihu.png"
+    }
+  ]
+}
